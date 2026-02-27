4c96c9f6f6a40889af73763802d14e335139e5d3
sushil singh <vandanasinghlko12@gmail.com>
2026-02-27 16:45:45 -0500

    first commit

---
+++ src/main/java/spring_jenkins/SpringJenkinsApplication.java
@@ -0,0 +1,14 @@
+package spring_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+		System.out.println("i am up and running");
+	}
+
+}

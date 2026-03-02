8cf0ad56d5ee3aa1e4be36460d2f83a2c6d876f0
sushil singh <vandanasinghlko12@gmail.com>
2026-03-02 16:22:57 -0500

    this is my second commit

--- src/main/java/spring_jenkins/SpringJenkinsApplication.java
+++ src/main/java/spring_jenkins/SpringJenkinsApplication.java
@@ -9,6 +9,7 @@ public class SpringJenkinsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJenkinsApplication.class, args);
 		System.out.println("i am up and running");
+		System.out.println("i am testing");
 	}
 
 }
